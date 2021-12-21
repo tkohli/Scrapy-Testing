013625a4702a5ca120ed0af98164e53278f09e25
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-21 22:13:07 +0530

    Working

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,12 +1,12 @@
 import scrapy
 
+
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://quotes.toscrape.com/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
