fc6ed08d1c921819d4f19f8eb05ee27b789c049a
Tanishka Kohli <35306595+tkohli@users.noreply.github.com>
2021-12-23 09:41:46 +0530

    Update quotes_spider.py

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from..items import TutorialItem
 
 
 class QuotesSpider(scrapy.Spider):
@@ -6,14 +7,29 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/'
+            'https://quotes.toscrape.com/'
         ]
+        # for i in range(1, 26):
+        #     urls.append(
+        #         "https://www.net-a-porter.com/en-in/shop/clothing/tops?pageNumber="+str(i))
+        # for i in range(1, 26):
+        #     urls.append(
+        #         "https://www.net-a-porter.com/en-in/shop/shoes?pageNumber="+str(i))
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        # title = response.css('title::text').get()
+        # yield {'title': title}
+        # for quote in range(50):
+        # items['Titles'] = title
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+            next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
